0141c44be2bbe6b3de0cbd1691e11d483119a4dc
Lokesh P S <ps.lokesh29@gmail.com>
2025-08-06 22:05:48 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/api/message", messageRoutes);
 // -------------------------------Deployment=---------------------------------
 
 const __dirname1 = path.resolve();
-
+require("dotenv").config();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
